d1c20527e238fda0e46cb680f5fb05a8cb71ff4c
Leandro Missel Jr <leandromss@hotmail.com>
2022-12-08 14:40:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use stirct';
+
+const sons = { 
+    'A': 'boom.wav',
+    'S': 'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'J': 'snare.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav',
+}
+
+const criarDiv = (texto) => { 
+    const div = document.createElement('div');
+    div.classList.add('key');
+    div.textContent = texto;
+    div.id = texto;
+    document.getElementById('container').appendChild(div);
+}
+
+const exibir = (sons) => {
+    Object.keys(sons).forEach(criarDiv); 
+}
+
+const tocarSom = (letra) => {
+    const audio = new Audio(`./assets/04-DRUM-KIT/sounds/${sons[letra]}`);
+    audio.play();
+}
+
+const adicionarEfeito = (letra) => document.getElementById(letra).classList.add('active');
+
+const removerEfeito = (letra) => {
+    const div = document.getElementById(letra);
+    const removeActive = () => div.classList.remove('active');
+    div.addEventListener('transitionend', removeActive);
+}
+
+const ativarDiv = (e) => { 
+    const letra = e.type == 'click' ? e.target.id : e.key.toUpperCase(); 
+    const letraPermitida = sons.hasOwnProperty(letra);
+    if(letraPermitida) {
+        adicionarEfeito(letra);
+        tocarSom(letra);
+        removerEfeito(letra);
+    }
+}
+
+exibir(sons);
+document.getElementById('container').addEventListener('click', ativarDiv);
+
+window.addEventListener('keydown', ativarDiv)
